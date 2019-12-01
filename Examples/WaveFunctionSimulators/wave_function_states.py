8a1d537cf4292de6c5c04181b02a63549d8c4fac
rakshith kunchum <settyblue@gmail.com>
2019-11-30 21:47:36 -0800

    Updated the wave function demo program.

--- Examples/WaveFunctionSimulators/wave_function_states.py
+++ Examples/WaveFunctionSimulators/wave_function_states.py
@@ -6,26 +6,34 @@ Created on Sat Nov 30 18:36:38 2019
 Simple program to demonstrate how to read the internal state of a wave function.
 For n qubits, the internal state will have 2^n dimensions and each dimensional
 value can be a complex number.
+Typical Output:
+Circuit:
+0: ───H───Y───
+
+1: ───H───Z───
+[ 0.-0.5j  0.+0.5j  0.+0.5j -0.-0.5j]
 """
 import cirq
 import numpy as np
 
 def main():
     circuit = cirq.Circuit()
-    qubit = cirq.LineQubit.range(2)
-    circuit.append(cirq.H(qubit[0]))
-    circuit.append([cirq.H(qubit[1]), cirq.Z(qubit[1])])
+    (q0, q1) = cirq.LineQubit.range(2)
+    circuit.append([cirq.H(q0), cirq.Y(q0)])
+    circuit.append([cirq.H(q1), cirq.Z(q1)])
     # Adding a measuring device will collapse the wave function.
     # This will not allow us to observe the state.
-    # circuit.append(cirq.measure(qubit[0], key= 'q0'))
+    # circuit.append(cirq.measure(q0, key= 'q0'))
 
     # So, in order to observe the state of the wave function, we will need
     # simulate without a measuring device.
 
     sim = cirq.Simulator()
-    result = sim.simulate(circuit)
+    # set the qubit order to correctly read the value of the multi-qubit
+    # state function.
+    result = sim.simulate(circuit, qubit_order = [q0, q1])
 
-    print("Circuit:\n", circuit)
+    print("Circuit:", circuit, sep='\n')
     print(np.around(result.final_state, 3))
 
 
